a79ef684d302edbb9c68b28911df02f53510db13
puhabg <puhabg@live.com>
2016-09-12 16:47:13 +0300

    Adds option to change the default avatar

--- Twitter/Web/Twitter.Web/App_Start/BundleConfig.cs
+++ Twitter/Web/Twitter.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -41,7 +42,6 @@
 
             bundles.Add(new StyleBundle("~/Content/avatar").Include(
                       "~/Content/site.avatar.css"));
-
         }
     }
 }
