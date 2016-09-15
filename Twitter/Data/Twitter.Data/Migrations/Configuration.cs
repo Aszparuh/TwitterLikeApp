c362d03c6caf5c687a72762ef28b995b275714ca
puhabg <puhabg@live.com>
2016-09-15 15:45:38 +0300

    Adds initial seed for admin user and avatar

--- Twitter/Data/Twitter.Data/Migrations/Configuration.cs
+++ Twitter/Data/Twitter.Data/Migrations/Configuration.cs
@@ -1,6 +1,15 @@
 ﻿namespace Twitter.Data.Migrations
 {
+    using System;
     using System.Data.Entity.Migrations;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Web;
+    using System.Web.Hosting;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
 
     public sealed class Configuration : DbMigrationsConfiguration<Twitter.Data.ApplicationDbContext>
     {
@@ -12,17 +21,63 @@
 
         protected override void Seed(Twitter.Data.ApplicationDbContext context)
         {
-            // This method will be called after migrating to the latest version.
+            if (!context.Users.Any())
+            {
+                var roleStore = new RoleStore<IdentityRole>(context);
+                var roleManager = new RoleManager<IdentityRole>(roleStore);
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
 
-            // You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
+                // Add missing roles
+                var role = roleManager.FindByName("Admin");
+                if (role == null)
+                {
+                    role = new IdentityRole("Admin");
+                    roleManager.Create(role);
+                }
+
+                // Create admin user
+                var user = userManager.FindByName("admin");
+                if (user == null)
+                {
+                    var newUser = new ApplicationUser()
+                    {
+                        UserName = "admin@admin.bg",
+                        Email = "admin@admin.bg",
+                        PhoneNumber = "5551234567",
+                    };
+
+                    string filePath = this.MapPath("~/App_Data/Resources/avatar.png");
+                    var image = System.Drawing.Image.FromFile(filePath);
+
+                    byte[] imageContent;
+                    using (var ms = new MemoryStream())
+                    {
+                        image.Save(ms, image.RawFormat);
+                        imageContent = ms.ToArray();
+                    }
+
+                    newUser.Avatar = new Image { FileName = "avatar", ContentType = "image/png", Content = imageContent, CreatedOn = DateTime.Now };
+
+                    userManager.Create(newUser, "Password1");
+                    userManager.SetLockoutEnabled(newUser.Id, false);
+                    userManager.AddToRole(newUser.Id, "Admin");
+                }
+            }
+        }
+
+        private string MapPath(string seedFile)
+        {
+            if (HttpContext.Current != null)
+            {
+                return HostingEnvironment.MapPath(seedFile);
+            }
+
+            var absolutePath = new Uri(Assembly.GetExecutingAssembly().CodeBase).AbsolutePath;
+            var directoryName = Path.GetDirectoryName(absolutePath);
+            var path = Path.Combine(directoryName, @"..\..\..\..\Web\Twitter.Web\" + seedFile.TrimStart('~').Replace('/', '\\'));
+
+            return path;
         }
     }
 }
