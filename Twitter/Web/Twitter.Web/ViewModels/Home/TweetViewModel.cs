922fa267db38028e45843a54f28b2bfe91941c2e
puhabg <puhabg@live.com>
2016-09-18 16:42:18 +0300

    Adds style for tweets on the homepage and adds Username to TweetViewModel

--- Twitter/Web/Twitter.Web/ViewModels/Home/TweetViewModel.cs
+++ Twitter/Web/Twitter.Web/ViewModels/Home/TweetViewModel.cs
@@ -1,12 +1,22 @@
 ﻿namespace Twitter.Web.ViewModels.Home
 {
+    using System;
+    using AutoMapper;
     using Data.Models;
     using Infrastructure.Mappings;
 
-    public class TweetViewModel : IMapFrom<Tweet>
+    public class TweetViewModel : IMapFrom<Tweet>, IHaveCustomMappings
     {
         public string Content { get; set; }
 
         public string ApplicationUserId { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public void CreateMappings(IMapperConfigurationExpression configuration)
+        {
+            configuration.CreateMap<Tweet, TweetViewModel>()
+                .ForMember(x => x.AuthorName, opt => opt.MapFrom(x => x.Author.UserName));
+        }
     }
 }
