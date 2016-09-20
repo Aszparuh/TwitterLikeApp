0ddef5e0f69b512f254106f73c405c758ae46dc2
puhabg <puhabg@live.com>
2016-09-20 17:18:08 +0300

    Adds input and model for creating new Tweet

---
+++ Twitter/Web/Twitter.Web/Controllers/TweetController.cs
@@ -0,0 +1,13 @@
+﻿namespace Twitter.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class TweetController : Controller
+    {
+        // GET: Tweet
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Twitter/Web/Twitter.Web/ViewModels/Tweet/CreateTweetViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Twitter.Web.ViewModels.Tweet
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CreateTweetViewModel
+    {
+        [Required]
+        [MaxLength(300)]
+        public string Content { get; set; }
+
+        public string ApplicationUserId { get; set; }
+    }
+}
