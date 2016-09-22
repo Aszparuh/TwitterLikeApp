5ac226a316a3e46050a94bfec982b2f01a093d2f
puhabg <puhabg@live.com>
2016-09-22 20:45:52 +0300

    Adds unobtrusive validation for the home page form

--- Twitter/Web/Twitter.Web/App_Start/BundleConfig.cs
+++ Twitter/Web/Twitter.Web/App_Start/BundleConfig.cs
@@ -9,11 +9,16 @@
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-unobtrusive").Include(
                         "~/Scripts/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval-unobtrusive").Include(
+                        "~/Scripts/jquery.validate.unobtrusive.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
