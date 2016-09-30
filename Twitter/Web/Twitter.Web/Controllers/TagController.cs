8b55f066260cce3bdad5d23ee32f70a17d288723
puhabg <puhabg@live.com>
2016-10-01 00:38:18 +0300

    Adds TagsRaplaceService

--- Twitter/Web/Twitter.Web/Controllers/TagController.cs
+++ Twitter/Web/Twitter.Web/Controllers/TagController.cs
@@ -16,12 +16,12 @@
             this.tags = tags;
         }
 
-        public ActionResult Index(string name)
+        public ActionResult Index(string id)
         {
             var tag = this.tags
                 .GetAll()
                 .Include(t => t.Tweets)
-                .FirstOrDefault(t => t.Name == name);
+                .FirstOrDefault(t => t.Name == id);
 
             if (tag == null)
             {
