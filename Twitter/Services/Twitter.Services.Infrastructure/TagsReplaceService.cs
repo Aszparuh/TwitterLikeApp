9767d9cf8c25dacd67133b0bf1338edfe0e21f2f
puhabg <puhabg@live.com>
2016-10-01 01:10:58 +0300

    Adds tests project and tests for TagsRaplaceService

---
+++ Twitter/Services/Twitter.Services.Infrastructure/TagsReplaceService.cs
@@ -0,0 +1,17 @@
+﻿namespace Twitter.Services.Infrastructure
+{
+    using System.Text.RegularExpressions;
+
+    public class TagsReplaceService
+    {
+        public string ReplaceHashtagsWithLinks(string originalContent)
+        {
+            return Regex.Replace(
+                originalContent,
+                "(#)((?:[A-Za-z0-9-_]*))",
+                c => string.Format(
+                    "<a href=\"/Tag/Index/%23{0}> </a>",
+                c.Value.TrimStart(new char[] { '#' })));
+        }
+    }
+}
---
+++ Twitter/Tests/Twitter.Tests/TagsReplaceServiceTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Twitter.Tests
+{
+    using NUnit.Framework;
+    using Services.Infrastructure;
+
+    [TestFixture]
+    public class TagsReplaceServiceTests
+    {
+        [Test]
+        public void TagsServiceReplaces_Tags_Correctly()
+        {
+            var tagReplaceService = new TagsReplaceService();
+            string text = "#Tag should become link";
+            string expected = "<a href=\"/Tag/Index/%23Tag> </a> should become link";
+
+            var result = tagReplaceService.ReplaceHashtagsWithLinks(text);
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [Test]
+        public void TagsServiceReplaces_Tags_Correctly_MultipleTags()
+        {
+            var tagReplaceService = new TagsReplaceService();
+            string text = "#Tag should #become #link";
+            string expected = "<a href=\"/Tag/Index/%23Tag> </a> should <a href=\"/Tag/Index/%23become> </a> <a href=\"/Tag/Index/%23link> </a>";
+
+            var result = tagReplaceService.ReplaceHashtagsWithLinks(text);
+
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
